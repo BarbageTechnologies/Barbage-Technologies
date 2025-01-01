e7d5a4a03ce013fbba4c2b8fc7f223741c4da207
Simon Keya <120686819+Simon-Keya@users.noreply.github.com>
2025-01-01 19:59:26 +0300

    commit

--- barbage-technologies/app/components/AboutSection.tsx
+++ barbage-technologies/app/components/AboutSection.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
 
 const AboutSection = () => {
   return (
     <section className="container mx-auto p-10">
       <h2 className="text-4xl font-bold">About Barbage Technologies</h2>
       <p className="mt-4">
-        Barbage Technologies is a cutting-edge technology company dedicated to developing innovative software solutions. Led by experienced software engineer and computer scientist Simon Keya, our team is committed to delivering high-quality products and services. 
+        Barbage Technologies is a cutting-edge technology company dedicated to developing innovative software solutions. Led by software engineer and computer scientist{' '}
+        <a href="https://simon-keya.vercel.app" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
+          Simon Keya
+        </a>, our team is committed to delivering high-quality products and services.
       </p>
       <p>
         We specialize in:
         <ul>
           <li>Mobile app development</li>
           <li>Web app development</li>
+          <li>Desktop app development</li>
           <li>AI and machine learning</li>
           <li>Data science</li>
+          <li>Cloud Computing</li>
         </ul>
       </p>
       <p>
